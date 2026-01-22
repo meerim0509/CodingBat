9eca4f7923ae9fbe26c9073e149d35cfe92be2bf
meerim0509 <massakunova@gmail.com>
2026-01-22 13:57:53 +0600

    Update from feature

--- src/conflict1/App1.java
+++ src/conflict1/App1.java
@@ -2,6 +2,6 @@ package conflict1;
 
 public class App1 {
     public static void main(String[] args) {
-        System.out.println("Hello from master");
+        System.out.println("Hello from feature");
     }
 }
